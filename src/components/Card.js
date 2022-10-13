78c94e6c551c07bd1d677e28743746313efefc10
Lucas <lucas.silva@kardbank.com.br>
2022-10-13 20:33:05 -0300

    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,11 @@ import styles from "../styles/Card.module.scss";
 
 const Card = ({ card, id, img }) => {
   return (
-    <a href={id} className={styles['card']} >
-       <img src={`${img}`}></img>
-    <h3>{id}</h3>
-   <p>Lorem ipsum dolor sit amet</p>
-   </a>
-   
+    <a href={id} className={styles["card"]}>
+      <img src={`${img}`}></img>
+      <h3>{id}</h3>
+      <p>Acessar</p>
+    </a>
   );
 };
 
